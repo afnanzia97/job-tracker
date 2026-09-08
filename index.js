953aea42b9642d5efeacc583328038bb29167ca6
afnanzia97 <afnanzia97@yahoo.com>
2026-09-08 16:58:07 +0100

    feat: add update and delete routes

--- index.js
+++ index.js
@@ -14,21 +14,27 @@ app.get('/api/health', (req, res) => {
   res.json({ status: 'ok' });
 });
 
-app.post('/api/jobs', async (req, res) => {
-  const job = await Job.create(req.body);
-  res.json(job);
-});
 
 app.post('/api/jobs', async (req, res) => {
   const job = await Job.create(req.body);
   res.json(job);
 }); 
 
-app.listen(5000, () => {
-  console.log('Server running on http://localhost:5000');
-});
 
 app.get('/api/jobs', async (req, res) => {
   const jobs = await Job.find();
   res.json(jobs);
+});
+
+app.delete('/api/jobs/:id', async (req, res) => {
+  const job = await Job.findByIdAndDelete(req.params.id);
+  res.json(job);
+});
+app.put('/api/jobs/:id', async (req, res) => {
+  const job = await Job.findByIdAndUpdate(req.params.id, req.body, { new: true });
+  res.json(job);
+});
+
+app.listen(5000, () => {
+  console.log('Server running on http://localhost:5000');
 });
