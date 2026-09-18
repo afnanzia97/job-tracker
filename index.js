736a765dabd0497f6d58d0bd8500df3017154b91
afnanzia97 <afnanzia97@yahoo.com>
2026-09-18 15:24:13 +0100

    chore: use environment port for deployment

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ app.get('/api/health', (req, res) => {
 
 app.use('/api/jobs', jobRoutes);
 
-app.listen(5000, () => {
-  console.log('Server running on http://localhost:5000');
-});
+const PORT = process.env.PORT || 5000;
 
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
